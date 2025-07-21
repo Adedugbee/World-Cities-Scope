f9bda8ef08db14fcaf92116807048ce81aa910fc
Adeduged <adedugbee@gmail.com>
2025-07-21 09:31:05 -0600

    Updated Join

--- SQL/Inner_Join_All_Tables.sql
+++ SQL/Inner_Join_All_Tables.sql
@@ -4,13 +4,16 @@ SELECT
   c.country,
   c.population_2024,
   c.population_2025,
-  l.`Average Life_Expectancy (UN 2024 Age)`,
-  l.`Female Life_Expectancy (UN 2024 Age)`,
-  l.`Male Life_Expectancy (UN 2024 Age)`,
+  l.`Average Life_Expectancy (UN 2024 Age)` AS avg_life_expectancy_2024,
+  l.`Female Life_Expectancy (UN 2024 Age)` AS female_life_expectancy_2024,
+  l.`Male Life_Expectancy (UN 2024 Age)` AS male_life_expectancy_2024,
   g.gdp_2023,
-  g.gdp_2024
+  g.gdp_2024,
+  cbg.gdp_billion_usd
 FROM top_200_cities c
+INNER JOIN cities_by_gdp cbg
+  ON c.city = cbg.city
 INNER JOIN UN_Life_Expectancy l
-  ON c.country = l.Country
+  ON c.country = l.country
 INNER JOIN world_gdp_2023_2024 g
   ON c.country = g.country;
